1cff30421e443214323f0d028bd0ca5a29b45152
abhilash2124 <abhilashaddagatla86@gmail.com>
2026-05-03 18:49:04 +0530

    added psycopg2

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_size=5, max_overflow=10)
+engine = create_engine(DATABASE_URL,
+                    pool_pre_ping=True, 
+                    pool_size=5, 
+                    max_overflow=10,
+                    connect_args={"sslmode": "require"})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
